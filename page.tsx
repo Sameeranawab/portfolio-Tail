8086f8b4b49045bf5e623355868d62b8ebbb67ca
Sameera Nawab <160266378+Sameeranawab@users.noreply.github.com>
2025-01-14 18:06:37 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
+import About from "@/components/About";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+import Project from "@/components/Project";
+
+export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-back",
+      duration: 1200,
+      delay: 100,
+      mirror: true,
+      anchorPlacement: "bottom-bottom",
+      offset: 160,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <main>
+      <Hero />
+      <Project />
+      <Skills />
+      <Contact />
+      <About />
+    </main>
+  );
+}
